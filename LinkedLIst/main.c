076cb5a831579f8780d73e0cd825f961620c9de2
Chiranjeev Kashyap <chiranjeevkashyap060@gmail.com>
2023-10-05 08:26:08 +0530

    changes on LinkedList

--- LinkedLIst/main.c
+++ LinkedLIst/main.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<conio.h>
 
 struct node {
     int data;
-    struct node *nextNode;
+    struct node *next;
 };
 
 // function declaration
@@ -13,22 +14,22 @@ void insertAtLast(struct node **root, int data) {
     struct node *tempNode, *traversalNode;
     tempNode = (struct node*)malloc(sizeof(struct node));
     tempNode->data = data;
-    tempNode->nextNode = NULL;
+    tempNode->next = NULL;
     if (*root == NULL)
         *root = tempNode;
     else {
         traversalNode = *root;
-        while(traversalNode->nextNode != NULL)
-            traversalNode = traversalNode->nextNode;
-        traversalNode->nextNode = tempNode;
+        while(traversalNode->next != NULL)
+            traversalNode = traversalNode->next;
+        traversalNode->next = tempNode;
     }
 }
 
 void insertAtFirst(struct node **root, int data) {
     struct node *tempNode = (struct node*)malloc(sizeof(struct node));
     tempNode->data = data;
-    tempNode->nextNode = NULL;
-    tempNode->nextNode = *root;
+    tempNode->next = NULL;
+    tempNode->next = *root;
     *root = tempNode;
 }
 
@@ -36,12 +37,83 @@ void afterAtNode(struct node **root, int data) {
     struct node *tempNode, *traversalNode;
     tempNode = (struct node*)malloc(sizeof(struct node));
     tempNode->data = data;
-    tempNode->nextNode = NULL;
+    tempNode->next = NULL;
+}
+
+void read(struct node *root) {
+    if (root == NULL) {
+        printf("NULL\n");
+    } else {
+        struct node *traversalNode = root;
+        while (traversalNode != NULL) {
+            printf("%d ",traversalNode->data);
+            if (traversalNode->next == NULL) {
+                traversalNode = NULL;
+            } else {
+                traversalNode = traversalNode->next;
+            }
+        }
+    }
+}
+
+int countNode(struct node *root) {
+    if (root == NULL) {
+        return -1;
+    } else {
+        int capacity = 0;
+        struct node *traversalNode = root;
+        while (traversalNode != NULL) {
+            traversalNode = traversalNode->next;
+            capacity++;
+        }
+        return capacity;
+    }
+}
+
+int menu() {
+    printf("1. Append Item\n");
+    printf("2. Insert Item\n");
+    printf("3. In Between\n");
+    printf("4. Exit\n\n");
+    printf("Enter your choice: ");
+    int choice;
+    scanf("%d",&choice);
+    return choice;
 }
 
 int main() {
     struct node *root = NULL;
-    insertAtLast(&root,25);
+    int data;
+    while (1) {
+        system("cls");
+        printf("Total Element: %d\n",countNode(root));
+        printf("List Elements: ");
+        read(root);
+        printf("\n");
+        switch(menu()) {
+            case 1:
+                printf("Enter item value to append: ");
+                scanf("%d",&data);
+                insertAtFirst(&root,data);
+                break;
+            case 2:
+                printf("Enter item value to insert: ");
+                scanf("%d",&data);
+                insertAtLast(&root,data);
+                break;
+            case 3:
+                printf("Under Construction");
+                break;
+            case 4:
+                printf("Press any key to exit: ");
+                getch();
+                exit(0);
+            default:
+                printf("Invalid choice, retry");
+                break;
+        }
+    }
+    getch();
     return 0;
 }
 
@@ -51,7 +123,7 @@ insert new node
 2) as a first node
 3) after a node
 
-define a node
+delete a node
 1) first node
 2) last node
 3) particular node
